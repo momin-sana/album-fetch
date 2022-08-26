25535167aeeb1201df07d7350e2ffa22fe790652
sanamomin17 <sana.m.momin@outlook.com>
2022-08-26 11:12:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import React from "react";
 import Home from "./views/Home";
-import { Route, BrowserRouter as Router, Routes, useParams } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Artist from "./views/Artist";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/artist-details/:id" element={<Artist />} />
-      </Routes>
-    </Router>
-  );
+    return ( <
+        Router >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/artist-details/:id"
+        element = { < Artist / > }
+        /> <
+        /Routes> <
+        /Router>
+    );
 }
 
 export default App;
