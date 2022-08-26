e96d9f11542f9094e8e5f5331b53572de9cdf2d5
sanamomin17 <sana.m.momin@outlook.com>
2022-08-26 11:12:06 +0500

    Update Artist.js

--- src/views/Artist.js
+++ src/views/Artist.js
@@ -2,27 +2,39 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Artist = () => {
-  let { id } = useParams();
-  const [artistDetails, setArtistDetails] = useState(null);
-  useEffect(() => {
-    const fetchList = () => {
-      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setArtistDetails(data);
-        });
-    };
-    fetchList();
-  }, []);
-  return (
-    <>
-      {artistDetails ? (
-        <div>{JSON.stringify(artistDetails)}</div>
-      ) : (
-        <div>Loading!</div>
-      )}
-    </>
-  );
+    let { id } = useParams();
+    const [artistDetails, setArtistDetails] = useState(null);
+    useEffect(() => {
+        const fetchList = () => {
+            fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setArtistDetails(data);
+                });
+        };
+        fetchList();
+    }, []);
+    return ( <
+        >
+        <
+        h1 style = {
+            {
+                margin: 5,
+                padding: 5,
+                textAlign: "center",
+                listStyle: "none",
+            }
+        } > Album Fetch < /h1>
+
+        {
+            artistDetails ? ( <
+                div > { JSON.stringify(artistDetails) } < /div>
+            ) : ( <
+                div > Loading! < /div>
+            )
+        } <
+        />
+    );
 };
 
 export default Artist;
